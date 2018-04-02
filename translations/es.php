d40a3cb8dddc71e6d5d398c08482d43a680b35c7
Johan Lantz <johan.lantz@telefonica.com>
2018-04-02 15:04:49 +0200

    initial commit, compat with sendy 3.0.x

---
+++ translations/es.php
@@ -0,0 +1,9 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{sendynewsletter}prestashop>sendynewsletter_198584454b0ce1101ff5b50323325aa8'] = 'Tu email';
+$_MODULE['<{sendynewsletter}prestashop>sendynewsletter_28798dc2341b3e18eb99be7df0f07c40'] = 'Hubo un error, por favor intenta de nuevo';
+$_MODULE['<{sendynewsletter}prestashop>sendynewsletter_e267e2be02cf3e29f4ba53b5d97cf78a'] = 'Tu email no es valido';
+$_MODULE['<{sendynewsletter}prestashop>sendynewsletter_5291e49637939e25eaf23d0f00897bd7'] = 'Ya estás suscrito';
+$_MODULE['<{sendynewsletter}prestashop>sendynewsletter_85cc8928cc95be27a2a10c87a429e14a'] = 'Suscripción exitosa';
